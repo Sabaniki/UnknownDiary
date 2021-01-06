5f107fbcc62aeb742f603a8f36be1162eff3a8c3
Sabaniki <sawada13@mccc.jp>
2021-01-06 13:33:17 +0900

    Bootstrapで適当に見た目を整え、herokuの準備も完了。

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,12 @@
 class Diary < ApplicationRecord
   belongs_to :created_day
+
+  def first_20chars
+    local_text = text
+    local_text.length <= 20 ? local_text : local_text[0, 20] + '...'
+  end
+
+  def created_day
+    CreatedDay.find(created_day_id).day
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'diaries#index'
   resources :diaries
   resources :created_days
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
